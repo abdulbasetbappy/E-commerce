06168cdca7218d74243275073aac8e611298ae09
abdulbasetbappy <abdulbasetbappy@hotmial.com>
2024-01-31 20:30:56 +0600

    Test HTTP Request and response

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        message: 'Test Successfull',
+    });
+});
+
 app.listen(3000, () => {
   console.log(`Server Listening On: http://localhost:3000`);
 });
